16d2205f86d203398c080f314927c86d89e77f00
Scarsrcool01 <jc.entp@gmail.com>
2019-11-04 12:11:12 -0500

    Added pre-up hooks

---
+++ hooks/pre-up/oh-my-zsh.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [ ! `command -v zsh` ]; then
+  echo "ZSH not found. Installing now."
+  brew install zsh
+fi
+
+if [ ! -e $HOME/.oh-my-zsh/ ]; then
+  echo "OH MY ZSH not found. Installing now."
+  curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
+fi
+
+if [ ! `command -v pry` ]; then
+  echo "PRY not found. Installing now"
+  gem install pry
+fi
