c718bafd0285d155800c80759b1e86a64be3ee18
Scarsrcool01 <jc.entp@gmail.com>
2020-01-13 10:02:33 -0500

    Added new_setup function and chsh

--- hooks/pre-up/oh-my-zsh.sh
+++ hooks/pre-up/oh-my-zsh.sh
@@ -1,21 +1,28 @@
 #!/bin/bash
+function new_setup(){
+  if [ ! `command -v zsh` ]; then
+    echo "ZSH not found. Installing now."
+    brew install zsh
+  fi
 
-if [ ! `command -v zsh` ]; then
-  echo "ZSH not found. Installing now."
-  brew install zsh
-fi
+  if [ ! -e $HOME/.oh-my-zsh/ ]; then
+    echo "OH MY ZSH not found. Installing now."
+    curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
+  fi
 
-if [ ! -e $HOME/.oh-my-zsh/ ]; then
-  echo "OH MY ZSH not found. Installing now."
-  curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
-fi
+  if [  `command -v zsh` ] && [ ! `echo $0` ]; then
+    echo "ZSH installation found. Switching shell to ZSH now."
+    chsh -s /bin/zsh
+    exec $SHELL -l
+  fi
 
-if [ ! `command -v pry` ]; then
-  echo "PRY not found. Installing now"
-  gem install pry
-fi
+  if [ ! `command -v pry` ]; then
+    echo "PRY not found. Installing now"
+    gem install pry
+  fi
 
-if [ ! `command -v geckodriver` ]; then
-  echo "GEKODRIVER not found. Installing now."
-  brew install geckodriver
-fi
+  if [ ! `command -v geckodriver` ]; then
+    echo "GEKODRIVER not found. Installing now."
+    brew install geckodriver
+  fi
+}
