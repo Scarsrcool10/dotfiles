06ac59f86d266591de1ea8858a461634a8f4b24c
Jon C <jon.cleghorn@coxautoinc.com>
2020-09-30 10:47:07 -0400

    standardizing some bash linting syntax

--- hooks/post-up/make_rc_files.sh
+++ hooks/post-up/make_rc_files.sh
@@ -1,44 +1,44 @@
 #!/bin/bash
 function make_syms(){
   GITFILE=$HOME/.gitconfig
-  if [ ! -f "${GITFILE}" ]; then
-    touch "${HOME}/.gitconfig"
-    ln -sf $HOME/dev/dotfiles/gitconfig $GITFILE
+  if [ ! -f "$GITFILE" ]; then
+    touch "$HOME/.gitconfig"
+    ln -sf "$HOME"/dev/dotfiles/gitconfig "$GITFILE"
   else
-    echo "${GITFILE} already exists."
+    echo "$GITFILE already exists."
   fi
 
   EXPORTS=$HOME/.exports
-  if [ ! -f "${EXPORTS}" ]; then
-    touch "${HOME}/.exports"
+  if [ ! -f "$EXPORTS" ]; then
+    touch "$HOME/.exports"
     CUSTOM_FF_PATH="export CUSTOM_FF_PATH=~/dev/cucumber-regression/Firefox\ 2.app/Contents/MacOS/firefox"
-    echo $CUSTOM_FF_PATH >> .exports
+    echo "$CUSTOM_FF_PATH" >> .exports
   else
-    echo "${EXPORTS} already exists."
+    echo "$EXPORTS already exists."
   fi
 
   PUB_ALIASES=$HOME/.pub_aliases
-  if [ ! -f "${PUB_ALIASES}" ]; then
-    touch "${HOME}/.pub_aliases"
-    ln -sf $HOME/dev/dotfiles/pub_aliases $PUB_ALIASES
+  if [ ! -f "$PUB_ALIASES" ]; then
+    touch "$HOME/.pub_aliases"
+    ln -sf "$HOME"/dev/dotfiles/pub_aliases "$PUB_ALIASES"
   else
     echo "${PUB_ALIASES} already exists."
   fi
 
   RCRC=$HOME/.rcrc
-  if [ ! -f "${RCRC}" ]; then
-    touch "${HOME}/.rcrc"
-    ln -sf $HOME/dev/dotfiles/rcrc $RCRC
+  if [ ! -f "$RCRC" ]; then
+    touch "$HOME/.rcrc"
+    ln -sf "$HOME"/dev/dotfiles/rcrc "$RCRC"
   else
-    echo "${RCRC} already exists."
+    echo "$RCRC already exists."
   fi
 
   PRYRC=$HOME/.pryrc
-  if [ ! -f "${PRYRC}" ]; then
-    touch "${HOME}/.pryrc"
-    ln -sf $HOME/dev/dotfiles/pryrc $PRYRC
+  if [ ! -f "$PRYRC" ]; then
+    touch "$HOME/.pryrc"
+    ln -sf "$HOME"/dev/dotfiles/pryrc "$PRYRC"
   else
-    echo "${PRYRC} already exists."
+    echo "$PRYRC already exists."
   fi
 
   BUNDLE_CONFIG=$HOME/.bundle/config
