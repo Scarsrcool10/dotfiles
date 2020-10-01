ffd9d291a7bae9f58932d74dc06e52ca35c6a8e5
Jon C <jon.cleghorn@coxautoinc.com>
2020-10-01 10:31:28 -0400

    Added force flags to make_syms func

--- hooks/post-up/make_rc_files.sh
+++ hooks/post-up/make_rc_files.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 function make_syms(){
   GITFILE=$HOME/.gitconfig
-  if [ ! -f "$GITFILE" ]; then
-    touch "$HOME/.gitconfig"
+  if [ "$1" = "force" ] || [ "$1" = "force_gitconfig" ]; then
     ln -sf "$HOME"/dev/dotfiles/gitconfig "$GITFILE"
+    echo "$GITFILE has been updated."
   else
-    echo "$GITFILE already exists."
+    if [ ! -f "$GITFILE" ]; then
+      touch "$HOME/.gitconfig"
+      ln -sf "$HOME"/dev/dotfiles/gitconfig "$GITFILE"
+    else
+      echo "$GITFILE already exists. Run with 'force' or 'force_gitconfig' to force link anyway."
+    fi
   fi
 
   EXPORTS=$HOME/.exports
@@ -18,39 +23,54 @@ function make_syms(){
   fi
 
   PUB_ALIASES=$HOME/.pub_aliases
-  if [ ! -f "$PUB_ALIASES" ]; then
-    touch "$HOME/.pub_aliases"
+  if [ "$1" = "force" ] || [ "$1" = "force_pub_aliases" ]; then
     ln -sf "$HOME"/dev/dotfiles/pub_aliases "$PUB_ALIASES"
+    echo "$PUB_ALIASES have been updated."
   else
-    echo "${PUB_ALIASES} already exists."
+    if [ ! -f "$PUB_ALIASES" ]; then
+      touch "$HOME/.pub_aliases"
+      ln -sf "$HOME"/dev/dotfiles/pub_aliases "$PUB_ALIASES"
+    else
+      echo "${PUB_ALIASES} already exists. Run with 'force' or 'force_pub_aliases' to force link anyway."
+    fi
   fi
 
   RCRC=$HOME/.rcrc
-  if [ ! -f "$RCRC" ]; then
-    touch "$HOME/.rcrc"
+  if [ "$1" = "force" ] || [ "$1" = "force_rcrc" ]; then
     ln -sf "$HOME"/dev/dotfiles/rcrc "$RCRC"
+    echo "$RCRC has been updated."
   else
-    echo "$RCRC already exists."
+    if [ ! -f "$RCRC" ]; then
+      touch "$HOME/.rcrc"
+      ln -sf "$HOME"/dev/dotfiles/rcrc "$RCRC"
+    else
+      echo "$RCRC already exists. Run with 'force' or 'force_rcrc' to force link anyway."
+    fi
   fi
 
   PRYRC=$HOME/.pryrc
-  if [ ! -f "$PRYRC" ]; then
-    touch "$HOME/.pryrc"
+  if [ "$1" = "force" ] || [ "$1" = "force_pryrc" ]; then
     ln -sf "$HOME"/dev/dotfiles/pryrc "$PRYRC"
+    echo "$PRYRC has been updated."
   else
-    echo "$PRYRC already exists."
+    if [ ! -f "$PRYRC" ]; then
+      touch "$HOME/.pryrc"
+      ln -sf "$HOME"/dev/dotfiles/pryrc "$PRYRC"
+    else
+      echo "$PRYRC already exists. Run with 'force' or 'force_pryrc' to force link anyway."
+    fi
   fi
 
   BUNDLE_CONFIG=$HOME/.bundle/config
   if [ "$1" = "force" ] || [ "$1" = "force_bundle_config" ]; then
     ln -sf "$HOME"/dev/dotfiles/bundle_config "$BUNDLE_CONFIG"
-    echo "$BUNDLE_CONFIG has been updated"
+    echo "$BUNDLE_CONFIG has been updated."
   else
     if [ ! -f "$BUNDLE_CONFIG" ]; then
       touch "$HOME/.bundle/config"
       ln -sf "$HOME"/dev/dotfiles/bundle_config "$BUNDLE_CONFIG"
     else
-      echo "$BUNDLE_CONFIG already exists"
+      echo "$BUNDLE_CONFIG already exists. Run with 'force' or 'force_bundle_config' to force link anyway."
     fi
   fi
 }
